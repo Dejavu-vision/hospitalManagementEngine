c7b3724d570b39410f5a810a44230483772dda13
prathmesh.kalburgi <prathmesh.kalburgi@payu.in>
2026-03-22 00:02:47 +0530

    cros error local fix v1

--- src/main/java/com/curamatrix/hsm/config/SecurityConfig.java
+++ src/main/java/com/curamatrix/hsm/config/SecurityConfig.java
@@ -75,7 +75,9 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList(
-            "http://localhost:*"
+            "http://localhost:3000",
+            "http://localhost:4200",
+            "http://localhost:5173"
         ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
